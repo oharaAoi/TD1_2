5059e365b1f63dde9ed06c4da9cd706e59b19ee2
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-02 23:18:56 +0900

    牛クラスにメンバ変数の追加

--- Game/Object/Cow/Cow.cpp
+++ Game/Object/Cow/Cow.cpp
@@ -1,17 +1,90 @@
 ﻿#include "Cow.h"
 
-Cow::Cow()
-{
+Cow::Cow() { Init(); }
+
+Cow::~Cow() { Finalize(); }
+
+
+/*==========================================================
+	初期化関数
+==========================================================*/
+void Cow::Init(){
+
+	// ワールド空間での中心座標
+	worldCenterPos_ = { 640.0f,360.0 };
+	size_ = { 32.0f,32.0f };
+	gh_ = Novice::LoadTexture("white1x1.png");
+
+	// ローカル空間での各頂点座標
+	localVertex_ = {
+		{ -size_.x * 0.5f, size_.y * 0.5f },
+		{ size_.x * 0.5f, size_.y * 0.5f },
+		{ -size_.x * 0.5f, -size_.y * 0.5f },
+		{ size_.x * 0.5f, -size_.y * 0.5f },
+	};
+
+	MakeWorldMatrix();
+
+	// ローカル空間以外の各行列
+	screenMatrix_ = worldMatrix_;
+
+	// ワールドとスクリーン空間での各頂点座標
+	screenVertex_ = worldVertex_;
 }
 
-void Cow::Init()
-{
+
+/*==========================================================
+	更新処理関数
+==========================================================*/
+void Cow::Update(){
+
+	// ワールド空間の行列と各頂点座標の計算
+	MakeWorldMatrix();
+
 }
 
-void Cow::Update()
-{
+
+/*==========================================================
+	描画処理関数
+==========================================================*/
+void Cow::Draw(){
+
+	Draw::Quad(screenVertex_, { 0.0f,0.0f }, { 1.0f,1.0f }, gh_, 0xFFFFFFFF);
+
 }
 
-void Cow::Draw()
-{
+
+/*==========================================================
+	終了処理関数
+==========================================================*/
+void Cow::Finalize() {
+
+
+}
+
+
+/*==========================================================
+	その他メンバ関数
+==========================================================*/
+
+void Cow::MatrixChange(const Matrix3x3& viewMatrix, const Matrix3x3& orthoMatrix, const Matrix3x3& viewportMatrix) {
+
+	screenMatrix_ = MakeWvpVpMatrix(worldMatrix_, viewMatrix, orthoMatrix, viewportMatrix);
+
+	screenVertex_.lt = Transform(worldVertex_.lt, worldMatrix_);
+	screenVertex_.rt = Transform(worldVertex_.rt, worldMatrix_);
+	screenVertex_.lb = Transform(worldVertex_.lb, worldMatrix_);
+	screenVertex_.rb = Transform(worldVertex_.rb, worldMatrix_);
+
+}
+
+void Cow::MakeWorldMatrix() {
+
+	worldMatrix_ = MakeAffineMatrix({ 1.0f,1.0f }, 0.0f, worldCenterPos_);
+
+	worldVertex_.lt = Transform(localVertex_.lt, worldMatrix_);
+	worldVertex_.rt = Transform(localVertex_.rt, worldMatrix_);
+	worldVertex_.lb = Transform(localVertex_.lb, worldMatrix_);
+	worldVertex_.rb = Transform(localVertex_.rb, worldMatrix_);
+
 }
--- Game/Object/Cow/Cow.h
+++ Game/Object/Cow/Cow.h
@@ -1,13 +1,56 @@
 ﻿#pragma once
-class Cow
-{
+
+#include <Novice.h>
+
+// ----- math ----- //
+#include "MyVector2.h"
+#include "Matrix3x3.h"
+#include "Quad.h"
+#include "Draw.h"
+
+class Cow final{
+private:
+	// member objcet
+
+	// ワールド座標での中心点
+	Vec2f worldCenterPos_;
+	Vec2f size_;
+	int gh_;
+
+	// 各空間での頂点座標
+	QuadVerf localVertex_;
+	QuadVerf worldVertex_;
+	QuadVerf screenVertex_;
+
+	// ワールドとスクリーン空間での行列
+	Matrix3x3 worldMatrix_;
+	Matrix3x3 screenMatrix_;
 
 public:
-
+	// Constructor & Destructor
 	Cow();
+	~Cow();
 
+	// default method
 	void Init();
 	void Update();
 	void Draw();
+	void Finalize();
+
+	// user method
+
+	// スクリーン行列と各頂点の計算
+	void MatrixChange(
+		const Matrix3x3& viewMatrix,
+		const Matrix3x3& orthoMatrix,
+		const Matrix3x3& viewportMatrix
+	);
+
+	// ワールド行列と各頂点の計算
+	void MakeWorldMatrix();
+
+
+	// accessor
+
 };
 
