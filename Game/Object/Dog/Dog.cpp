661ddb125cb040b3e3f61d8710e89b6d7efbaa34
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-02 22:56:37 +0900

    犬クラスのメンバ変数の追加 ローカル、ワールド、スクリーンでの各頂点座標、 ワールド、スクリーンの行列

--- Game/Object/Dog/Dog.cpp
+++ Game/Object/Dog/Dog.cpp
@@ -1,17 +1,95 @@
 ﻿#include "Dog.h"
 
-Dog::Dog() {
+Dog::Dog() { Init(); }
 
-}
+Dog::~Dog() { Finalize(); }
 
+
+/*==========================================================
+	メンバ変数の初期化関数
+==========================================================*/
 void Dog::Init() {
 
+	// ワールド空間での中心座標
+	worldCenterPos_ = { 640.0f,360.0 };
+	size_ = { 32.0f,32.0f };
+	gh_ = Novice::LoadTexture("white1x1.png");
+
+	// ローカル空間での各頂点座標
+	localVertex_ = {
+		{ -size_.x * 0.5f, size_.y * 0.5f },
+		{ size_.x * 0.5f, size_.y * 0.5f },
+		{ -size_.x * 0.5f, -size_.y * 0.5f },
+		{ size_.x * 0.5f, -size_.y * 0.5f },
+	};
+
+	MakeWorldMatrix();
+
+	// ローカル空間以外の各行列
+	screenMatrix_ = worldMatrix_;
+
+	// ワールドとスクリーン空間での各頂点座標
+	screenVertex_ = worldVertex_;
+
+
 }
 
+
+/*==========================================================
+	更新処理関数
+==========================================================*/
 void Dog::Update() {
 
+
+	// ワールド空間の行列と各頂点座標の計算
+	MakeWorldMatrix();
+
 }
 
+
+/*==========================================================
+	描画処理関数
+==========================================================*/
 void Dog::Draw() {
 
+	// 犬の描画
+	Draw::Quad(screenVertex_, { 0.0f,0.0f }, { 1.0f,1.0f }, gh_, 0xFFFFFFFF);
+
 }
+
+
+/*==========================================================
+	終了処理関数
+==========================================================*/
+void Dog::Finalize() {
+
+
+}
+
+
+/*==========================================================
+	その他メンバ関数
+==========================================================*/
+
+void Dog::MatrixChange(const Matrix3x3& viewMatrix, const Matrix3x3& orthoMatrix, const Matrix3x3& viewportMatrix) {
+
+	screenMatrix_ = MakeWvpVpMatrix(worldMatrix_, viewMatrix, orthoMatrix, viewportMatrix);
+
+	screenVertex_.lt = Transform(worldVertex_.lt, worldMatrix_);
+	screenVertex_.rt = Transform(worldVertex_.rt, worldMatrix_);
+	screenVertex_.lb = Transform(worldVertex_.lb, worldMatrix_);
+	screenVertex_.rb = Transform(worldVertex_.rb, worldMatrix_);
+
+}
+
+void Dog::MakeWorldMatrix() {
+	worldMatrix_ = MakeAffineMatrix({ 1.0f,1.0f }, 0.0f, worldCenterPos_);
+
+	worldVertex_.lt = Transform(localVertex_.lt, worldMatrix_);
+	worldVertex_.rt = Transform(localVertex_.rt, worldMatrix_);
+	worldVertex_.lb = Transform(localVertex_.lb, worldMatrix_);
+	worldVertex_.rb = Transform(localVertex_.rb, worldMatrix_);
+
+}
+
+
--- Game/Object/Dog/Dog.h
+++ Game/Object/Dog/Dog.h
@@ -1,14 +1,57 @@
 ﻿#pragma once
-class Dog
-{
+
+#include <Novice.h>
+
+// ----- math ----- //
+#include "MyVector2.h"
+#include "Matrix3x3.h"
+#include "Quad.h"
+#include "Draw.h"
+
+class Dog {
+private:
+	// member object
+
+	// ワールド空間での中心座標
+	Vec2f worldCenterPos_;
+	Vec2f size_;
+	int gh_;
+
+	// 各空間の頂点座標
+	QuadVerf localVertex_;
+	QuadVerf worldVertex_;
+	QuadVerf screenVertex_;
+	
+	// ローカル空間以外の各行列
+	Matrix3x3 worldMatrix_;
+	Matrix3x3 screenMatrix_;
+
 
 public:
-
+	// Constructor & Destructor
 	Dog();
+	~Dog();
 
+	// default method
 	void Init();
 	void Update();
 	void Draw();
+	void Finalize();
+
+	// user method
+
+	// スクリーン空間に変更するための関数
+	void MatrixChange(
+		const Matrix3x3& viewMatrix,
+		const Matrix3x3& orthoMatrix,
+		const Matrix3x3& viewportMatrix
+	);
+
+	// ワールド空間の行列と各頂点の計算
+	void MakeWorldMatrix();
+
+	// accessor
+
 
 };
 
