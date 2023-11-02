bf01c7533ab04e6adc220a9fee1f65d11b16294e
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-02 23:47:09 +0900

    レベルセレクトシーンクラスの追加 シーンマネージャーには未追加

---
+++ Game/Scene/Scene_LevelSelect.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <Novice.h>
+
+// ----- base ----- //
+#include "BaseScene.h"
+
+
+class Scene_LevelSelect : public BaseScene {
+private:
+	// member object
+
+
+
+public:
+	// Constructor & Destructor
+	Scene_LevelSelect();
+	~Scene_LevelSelect();
+
+	// default method
+	void Init() override;
+	void Update() override;
+	void Draw() override;
+	void Finalize() override;
+
+};
+
