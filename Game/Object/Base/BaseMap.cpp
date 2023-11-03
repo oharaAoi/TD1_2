b2990cf80c32ff1d0f92d75da845516e754fdd31
oharaAoi <k023g0108@g.neec.ac.jp>
2023-11-03 10:56:06 +0900

    baseMapの追加

---
+++ Game/Object/Base/BaseMap.cpp
@@ -0,0 +1,34 @@
+﻿#include "BaseMap.h"
+
+//===========================================================================
+//　静的メンバ変数の初期化
+std::string BaseMap::csvFilePath_ = "./Resources/stage/stageTest1.csv";
+std::vector<std::vector<int>> BaseMap::mapAdd_ = LoadFile(csvFilePath_);
+
+int BaseMap::row_ = static_cast<int>(mapAdd_.size());
+int BaseMap::col_ = static_cast<int>(mapAdd_[0].size());
+
+Vec2f BaseMap::tileSize_ = { 64.0f, 64.0f };
+
+int BaseMap::GH_ = 0;
+
+//===========================================================================
+
+BaseMap::BaseMap() {
+	Init();
+}
+
+BaseMap::~BaseMap() {}
+
+//===========================================================================
+void BaseMap::AddressRevers(){
+	std::reverse(mapAdd_.begin(), mapAdd_.end());
+}
+
+//===========================================================================
+void BaseMap::Init(){
+	AddressRevers();
+	GH_ = Novice::LoadTexture("./Resources/images/mapTile/colorMap.png");
+}
+
+
---
+++ Game/Object/Base/BaseMap.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+#include <Novice.h>
+
+/* lib */
+#include "MyVector2.h"
+#include "Lib/LoadFile/LoadFile.h"
+
+class BaseMap
+{
+protected:
+	//==================================================
+	// マップのアドレス
+	static std::vector<std::vector<int>>mapAdd_;
+
+	// マップのファイルパス
+	static std::string csvFilePath_;
+
+	// 行と列
+	static int row_;
+	static int col_;
+
+	// タイルのサイズ
+	static Vec2f tileSize_;
+
+	// リソース
+	static int GH_;
+
+	//==================================================
+
+public:
+	// コンストラクタ
+	BaseMap();
+
+	static void AddressRevers();
+	static void Init();
+
+	//*ここから先は継承先でも使う*/
+	// デストラクタ
+	virtual ~BaseMap();
+
+};
+
