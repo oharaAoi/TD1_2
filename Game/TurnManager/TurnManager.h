3207d661a70595cbd7aedfd045d21c1dbc3c3739
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-05 22:33:55 +0900

    ターンを管理するクラスの追加

---
+++ Game/TurnManager/TurnManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+
+class TurnManager {
+private:
+
+	int turnNo_;
+	bool isTurnChange_;
+
+public:
+	// Constructor & Destuctor
+	TurnManager();
+	~TurnManager();
+
+	// default method
+	void Init();
+	void Update();
+	void Draw();
+	void Finalize();
+
+};
+
