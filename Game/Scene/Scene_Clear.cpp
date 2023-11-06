ace12a014637022c266e6a69838b4d50c60087d5
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-06 13:20:19 +0900

    タイトルへの遷移を追加

--- Game/Scene/Scene_Clear.cpp
+++ Game/Scene/Scene_Clear.cpp
@@ -1,14 +1,13 @@
 ﻿#include "Scene_Clear.h"
 
-Scene_Clear::Scene_Clear(){}
+Scene_Clear::Scene_Clear() {}
 
-Scene_Clear::~Scene_Clear(){}
+Scene_Clear::~Scene_Clear() {}
 
 /*================================================================
 	初期化関数
 ================================================================*/
-void Scene_Clear::Init()
-{
+void Scene_Clear::Init() {
 
 
 
@@ -17,23 +16,22 @@ void Scene_Clear::Init()
 /*================================================================
 	更新処理関数
 ================================================================*/
-void Scene_Clear::Update()
-{
+void Scene_Clear::Update() {
 
-
+	if (input->IsTriggerKey(DIK_RETURN)) {
+		nextSceneNo_ = SCENE::TITLE;
+	}
 
 }
 
 /*================================================================
 	描画処理関数
 ================================================================*/
-void Scene_Clear::Draw()
-{
+void Scene_Clear::Draw() {
 
 	Novice::ScreenPrintf(10, 60, "GameClear");
 
 }
 
-void Scene_Clear::Finalize()
-{
+void Scene_Clear::Finalize() {
 }
